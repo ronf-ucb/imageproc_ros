1c17a2dd1bc543c9224375e295c6f2478bb9b453
Andrew Chen <andrew.chen@berkeley.edu>
2013-03-23 22:12:35 -0700

    added teleop with additional parameters and run-stop

--- scripts/teleop.py
+++ scripts/teleop.py
@@ -37,8 +37,8 @@ roslib.load_manifest('imageproc_ros')
 import rospy
 import geometry_msgs.msg
 from imageproc import serial_comm
-pos_gain = 1200   # straight throttle component
-turn_gain = 1200   # turn gain component
+pos_gain = 2000   # straight throttle component
+turn_gain = 2000   # turn gain component
 import time
 
 def handle_command(msg, robotname):
@@ -47,12 +47,19 @@ def handle_command(msg, robotname):
     print 'robot = ' + robotname
     left_throttle = pos_gain * msg.linear.x - turn_gain * msg.angular.z
     right_throttle = pos_gain * msg.linear.x + turn_gain * msg.angular.z
+    left_throttle =  -left_throttle if invertLeft else left_throttle
+    right_throttle = -right_throttle if invertRight else right_throttle
+    left_throttle = clamp(left_throttle, minThrottle, maxThrottle)
+    right_throttle = clamp(right_throttle, minThrottle, maxThrottle)
     print 'setting thrust left=%d  right=%d' %(left_throttle, right_throttle)
     serial.setThrust(left_throttle, right_throttle, 100)
-    
+
+def clamp(value, minVal, maxVal):
+    return max(minVal, min(maxVal, value)) 
     
 if __name__ == '__main__':
     global serial
+    global invertLeft, invertRight, minThrottle, maxThrottle
     rospy.init_node('teleop')
 
     # add default value
@@ -62,7 +69,12 @@ if __name__ == '__main__':
                      handle_command,
                      robotname)
 
-    device = rospy.get_param('~device', '/dev/ttyUSB1')
+    device = rospy.get_param('~device', '/dev/ttySAC1')
+    invertLeft = rospy.get_param('~invertLeft', True)
+    invertRight = rospy.get_param('~invertRight', False)
+    minThrottle = rospy.get_param('~minThrottle', -4000)
+    maxThrottle = rospy.get_param('~maxThrottle', 4000)
+
 
     try:
         print 'initializing robot'
