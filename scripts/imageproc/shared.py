d78dc91c30e6c3d894ce1e30014f932f4a216797
ronf <ronf@eecs.berkeley.edu>
2013-05-31 11:44:39 +0200

    track packet numbers

--- scripts/imageproc/shared.py
+++ scripts/imageproc/shared.py
@@ -7,7 +7,8 @@ steering_gains_set = False
 steering_rate_set = False
 robot_ready = False
 
-pkts = 0
+pkts = 0  # count of packets received
+telem_index = 0   # count of packets sent by ImageProc since last reset
 count2deg = 2000.0/(2**15-1)
 
 imudata = []
